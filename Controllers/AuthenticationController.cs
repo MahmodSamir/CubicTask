f1b6feafa9b6941dea9088eed461cfc8998bce2d
mahmoud samir <mahmoudsamirms01@gmail.com>
2024-05-27 20:26:16 +0300

    Auth controller to mock a valid user and Test controller with authorized and public endpoints

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+	private readonly IConfiguration _configuration;
+
+	public AuthenticationController(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
+
+	[HttpPost("login")]
+	public IActionResult Login([FromBody] LoginModel login)
+	{
+		if (login.Username == "CubicUser" && login.Password == "CubicPassword")
+		{
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var key = Encoding.ASCII.GetBytes(_configuration["JWT:Key"]);
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(new Claim[]
+				{
+					new Claim(ClaimTypes.Name, login.Username)
+				}),
+				Expires = DateTime.UtcNow.AddHours(1),
+				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+				Issuer = _configuration["JWT:Issuer"],
+				Audience = _configuration["JWT:Audience"]
+			};
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+			var tokenString = tokenHandler.WriteToken(token);
+
+			return Ok(new { Token = tokenString });
+		}
+
+		return Unauthorized();
+	}
+}
+
+public class LoginModel
+{
+	public string Username { get; set; }
+	public string Password { get; set; }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+	[HttpGet("secure")]
+	[Authorize]
+	public IActionResult GetSecureData()
+	{
+		return Ok("Hello Cubic user, you are authenticated, authorized, and secured.");
+	}
+
+	[HttpGet("public")]
+	public IActionResult GetPublicData()
+	{
+		return Ok("It's a public data no authentication required. So, it's not secured.");
+	}
+}
